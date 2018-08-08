e175f0f664f39497fbcfd41fcdd029c67b6b578a
lessless <yevhenii.kurtov@gmail.com>
2018-08-08 20:27:59 +0700

    Added dialyxir for improved spec verification

--- mix.exs
+++ mix.exs
@@ -46,6 +46,7 @@ defmodule ExAws.SES.Mixfile do
       {:hackney, ">= 0.0.0", only: [:dev, :test]},
       {:sweet_xml, ">= 0.0.0", only: [:dev, :test]},
       {:poison, ">= 0.0.0", only: [:dev, :test]},
+      {:dialyxir, "~> 1.0.0-rc.3", only: [:dev], runtime: false},
       ex_aws(),
     ]
   end
